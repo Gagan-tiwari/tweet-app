417b3cc348c4ed22f92f31be984b2ef3153231f9
Gagan <gagantiwari370@gmail.com>
2024-09-14 16:49:45 +0530

    added some settings

--- build_file.sh
+++ build_file.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 python3.9 -m venv .venv
-source .venv/bin/activate 
+source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
