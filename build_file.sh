5ec6fb74e9c0ffa21d7f01c222d3f958b93b8702
Gagan <gagantiwari370@gmail.com>
2024-09-14 16:38:42 +0530

    added runtime file

--- build_file.sh
+++ build_file.sh
@@ -1,7 +1,6 @@
 echo "BUILD START"
 python3.9 -m venv .venv
-source .venv/bin/activate
-apt-get update && apt-get install -y python3-pip
+source .venv/bin/activate 
 pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
