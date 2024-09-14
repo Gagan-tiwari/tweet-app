4de7133de70ad3fea77de319318ccac5c7a08c09
Gagan <gagantiwari370@gmail.com>
2024-09-14 16:13:37 +0530

    build changes

--- build_file.sh
+++ build_file.sh
@@ -1,2 +1,6 @@
+echo "BUILD START"
+python3.9 -m venv .venv
+.venv/script/activate
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput
+echo "BUILD END"
